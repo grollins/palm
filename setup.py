34e888c99827177f6cc7ef0f64ff01f41e2f634a
Geoff Rollins <grollins@gmail.com>
2012-12-20 15:18:21 -0800

    Corrected setup script.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ def configuration(parent_package='', top_path=None):
                        quiet=False)
 
     config.add_subpackage('palm')
+    config.add_data_dir('scripts')
 
     return config
 
