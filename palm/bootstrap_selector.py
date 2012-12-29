cbd4a28936d981c7e11127429d7bf044dbc5962e
Geoff Rollins <grollins@gmail.com>
2012-12-29 12:10:13 -0600

    Fixed bootstrap selector.

--- palm/bootstrap_selector.py
+++ palm/bootstrap_selector.py
@@ -8,10 +8,10 @@ class BootstrapSelector(base.data_selector.DataSelector):
 
     def select_data(self, target_data, size):
         n = len(target_data)
-        assert size <= n
-        bs = Bootstrap(n, 1, train_size=size)
+        assert size <= n-1
+        bs = Bootstrap(n, 1, train_size=n-1)
         train_index, test_index = bs.__iter__().next()
         train_index = list(train_index)
-        inds = train_index
+        inds = train_index[:size]
         new_target_data = target_data.make_copy_from_selection(inds)
         return new_target_data
