9d68ddacc362dcb22ee7797d49baafdcb9a6371a
Geoff Rollins <grollins@gmail.com>
2013-03-08 17:44:39 -0800

    More optimization arguments that affect convergence.

--- palm/scipy_optimizer.py
+++ palm/scipy_optimizer.py
@@ -7,14 +7,25 @@ class ScipyOptimizer(ParameterOptimizer):
     parameter values. Uses Scipy optimization routines.
     Currently using a bounded BFGS routine.
     """
-    def __init__(self):
+    def __init__(self, factr=1e6, pgtol=1e-5, epsilon=1e-8, maxfun=1000):
         super(ScipyOptimizer, self).__init__()
         self.optimization_fcn = scipy.optimize.fmin_l_bfgs_b
+        self.factr = factr
+        self.pgtol = pgtol
+        self.epsilon = epsilon
+        self.maxfun = maxfun
 
-    def optimize_parameters(self, score_fcn, parameter_set):
+    def optimize_parameters(self, score_fcn, parameter_set, noisy=False):
         bounds = parameter_set.get_parameter_bounds()
+        if noisy:
+            iprint = 1
+        else:
+            iprint = -1
         results = self.optimization_fcn(score_fcn, x0=parameter_set.as_array(),
-                                        bounds=bounds, approx_grad=1)
+                                        bounds=bounds, approx_grad=1,
+                                        iprint=iprint,
+                                        factr=self.factr, pgtol=self.pgtol,
+                                        epsilon=self.epsilon, maxfun=self.maxfun)
         optimal_parameter_array = results[0]
         parameter_set.update_from_array(optimal_parameter_array)
         score = float(results[1])
--- palm/score_function.py
+++ palm/score_function.py
@@ -1,14 +1,17 @@
 class ScoreFunction(object):
     def __init__(self, model_factory, parameter_set, judge, data_predictor,
-                 target_data):
+                 target_data, noisy=False):
         self.model_factory = model_factory
         self.parameter_set = parameter_set
         self.judge = judge
         self.data_predictor = data_predictor
         self.target_data = target_data
+        self.noisy = noisy
     def compute_score(self, current_parameter_array):
         self.parameter_set.update_from_array(current_parameter_array)
         current_model = self.model_factory.create_model(self.parameter_set)
         score = self.judge.judge_prediction(current_model, self.data_predictor,
                                             self.target_data)
+        if self.noisy:
+            print "%.6f,%s" % (score, self.parameter_set)
         return score
