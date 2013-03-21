19f19104a14031fb36754188bbadfa6afbf4ec83
Geoff Rollins <grollins@gmail.com>
2013-03-20 22:45:14 -0700

    Refactor kinetic models to incorporate changes to rate matrix, state enumerator, and route mapper. All three use pandas now.

--- palm/aggregated_kinetic_model.py
+++ palm/aggregated_kinetic_model.py
@@ -1,7 +1,9 @@
 import numpy
 from collections import defaultdict
 from palm.base.model import Model
-from palm.rate_matrix import RateMatrixFactory, AggregatedRateMatrix
+from palm.rate_matrix import make_rate_matrix_from_state_ids
+from palm.state_collection import StateIDCollection
+from palm.rate_fcn import rate_from_rate_id
 
 class State(object):
     '''
@@ -27,31 +29,41 @@ class State(object):
     def is_initial_state(self):
         return self.initial_state_flag
 
+    def as_dict(self):
+        return {'observation_class':self.get_class()}
+
+
 class Route(object):
     '''
     A generic route class for aggregated kinetic models.
     '''
-    def __init__(self, start_state, end_state, log_rate_function,
-                 label=''):
-        self.start_state = start_state
-        self.end_state = end_state
-        self.log_rate_function = log_rate_function
-        self.label = label
+    def __init__(self, id_str, start_state_id, end_state_id, rate_id,
+                 multiplicity):
+        self.id = id_str
+        self.start_state_id = start_state_id
+        self.end_state_id = end_state_id
+        self.rate_id = rate_id
+        self.multiplicity = multiplicity
 
     def __str__(self):
-        my_str = "%s %s %s %.3e" % (self.label, self.start_state,
-                                    self.end_state,
-                                    self.log_rate_function(t=0.0))
+        my_str = "%s %s %s %s %d" % (
+                    self.id, self.start_state_id, self.end_state_id,
+                    self.rate_id, self.multiplicity)
         return my_str
 
-    def get_label(self):
-        return self.label
-
+    def get_id(self):
+        return self.id
     def get_start_state(self):
-        return self.start_state
-
-    def compute_log_rate(self, t):
-        return self.log_rate_function(t)
+        return self.start_state_id
+    def get_end_state(self):
+        return self.end_state_id
+    def get_multiplicity(self):
+        return self.multiplicity
+    def as_dict(self):
+        return {'start_state':self.start_state_id,
+                'end_state':self.end_state_id,
+                'rate_id':self.rate_id,
+                'multiplicity':self.multiplicity}
 
 
 class AggregatedKineticModel(Model):
@@ -65,96 +77,52 @@ class AggregatedKineticModel(Model):
         self.state_enumerator = state_enumerator
         self.route_mapper = route_mapper
         self.parameter_set = parameter_set
-        self.rate_matrix_factory = RateMatrixFactory(AggregatedRateMatrix)
 
-        unsorted_state_list = self.state_enumerator()
-        sorting_results = self._sort_states_by_observation_class(unsorted_state_list)
-        self.states = sorting_results[0]
-        self.state_index_dict = sorting_results[1]
-        self.class_indices_dict = sorting_results[2]
+        self.state_collection, self.initial_state_id = self.state_enumerator()
+        self.state_groups = self.state_collection.sort('observation_class')
+        self.state_id_collection = self.state_collection.get_state_ids()
+        self.state_ids_by_class_dict = {}
+        for obs_class, id_list in self.state_groups.groups.iteritems():
+            this_state_id_collection = StateIDCollection()
+            this_state_id_collection.from_state_id_list(id_list)
+            self.state_ids_by_class_dict[obs_class] = this_state_id_collection
 
-        self.routes = self.route_mapper(self.states, self.state_index_dict)
+        self.route_collection = self.route_mapper(self.state_collection)
         self.rate_matrix = None
-
-    def _sort_states_by_observation_class(self, unsorted_state_list):
-        # initialize data structures for state sorting
-        unsorted_state_index_dict = {}
-        unsorted_class_indices_dict = defaultdict(list)
-        sorted_state_list = []
-        sorted_state_index_dict = {}
-        sorted_class_indices_dict = defaultdict(list)
-
-        # store current state indices by class name and by state id
-        for this_index, this_state in enumerate(unsorted_state_list):
-            unsorted_state_index_dict[this_state.get_id()] = this_index
-            unsorted_class_indices_dict[this_state.get_class()].append(this_index)
-
-        # sort states by class name
-        sorted_index = 0
-        for key, value in unsorted_class_indices_dict.iteritems():
-            this_class_indices_list = value
-            sorted_indices_list = []
-            for index in this_class_indices_list:
-                this_state = unsorted_state_list[index]
-                sorted_state_list.append(this_state)
-                sorted_indices_list.append(sorted_index)
-                sorted_index += 1
-            sorted_class_indices_dict[key] = sorted_indices_list
-
-        # store sorted state indices by class name and by state id
-        for this_index, this_state in enumerate(sorted_state_list):
-            sorted_state_index_dict[this_state.get_id()] = this_index
-
-        # save index of initial state
-        for this_index, this_state in enumerate(sorted_state_list):
-            if this_state.is_initial_state():
-                self.initial_state_index = this_index
-
-        # return sorted states
-        return sorted_state_list, sorted_state_index_dict,\
-               sorted_class_indices_dict
-
-    def iter_states(self):
-        for s in self.states:
-            yield s
-
-    def iter_routes(self):
-        for r in self.routes:
-            yield r
 
     def get_parameter(self, parameter_name):
         return self.parameter_set.get_parameter(parameter_name)
 
     def get_num_states(self, class_name=None):
         if class_name:
-            return len(self.class_indices_dict[class_name])
+            return len(self.state_groups[class_name])
         else:
-            return len(self.states)
+            return len(self.state_collection)
 
     def get_num_routes(self):
-        return len(self.routes)
+        return len(self.route_collection)
 
     def build_rate_matrix(self, time=0.):
-        rate_matrix = self.rate_matrix_factory.create_rate_matrix(
-                            self.get_num_states(), self.routes,
-                            self.state_index_dict, self.class_indices_dict,
-                            time)
-        self.rate_matrix = rate_matrix
+        # need a state id collection
+        rate_matrix = make_rate_matrix_from_state_ids(self.state_id_collection)
+        for r_id, r in self.route_collection.iter_routes():
+            start_id = r['start_state']
+            end_id = r['end_state']
+            rate_id = r['rate_id']
+            multiplicity = r['multiplicity']
+            this_rate = multiplicity * rate_from_rate_id(rate_id, time,
+                                                         self.parameter_set)
+            assert this_rate >= 0.0, "%s %s %.2e" % (start_id, end_id, this_rate)
+            rate_matrix.set_rate(start_id, end_id, this_rate)
+        rate_matrix.balance_transition_rates()
+        return rate_matrix
 
-    def get_numpy_submatrix(self, start_class, end_class):
-        return self.get_submatrix(start_class, end_class)
-
-    def get_numpy_full_matrix(self):
-        return self.get_full_matrix()
-
-    def get_submatrix(self, start_class, end_class):
-        submatrix = self.rate_matrix_factory.create_submatrix(
-                        self.rate_matrix, start_class, end_class)
+    def get_submatrix(self, rate_matrix, start_class, end_class):
+        start_id_collection = self.state_ids_by_class_dict[start_class]
+        end_id_collection = self.state_ids_by_class_dict[end_class]
+        submatrix = rate_matrix.get_submatrix(
+                        start_id_collection, end_id_collection)
         return submatrix
 
-    def get_full_matrix(self):
-        return self.rate_matrix
-
-    def get_local_submatrix(self, start_class, end_class, state_list, depth=3):
+    def get_local_matrix(self, start_state, depth=3):
         return None
-        
--- palm/blink_model.py
+++ palm/blink_model.py
@@ -1,6 +1,7 @@
 import numpy
 from palm.aggregated_kinetic_model import AggregatedKineticModel
 from palm.probability_vector import make_prob_vec_from_state_ids
+from palm.state_collection import StateIDCollection
 
 class SingleDarkState(object):
     '''
@@ -15,24 +16,21 @@ class SingleDarkState(object):
         self.B = B
         self.observation_class = observation_class
         self.initial_state_flag = False
-
     def __str__(self):
         return "%s %s" % (self.id, self.observation_class)
-
     def as_array(self):
         return numpy.array([self.I, self.A, self.D, self.B])
-
     def get_id(self):
         return self.id
-
     def get_class(self):
         return self.observation_class
-
     def is_initial_state(self):
         return self.initial_state_flag
-
     def set_initial_state_flag(self):
         self.initial_state_flag = True
+    def as_dict(self):
+        return {'observation_class':self.get_class(),
+                'I':self.I, 'A':self.A, 'D':self.D, 'B':self.B}
 
 
 class DoubleDarkState(object):
@@ -49,43 +47,22 @@ class DoubleDarkState(object):
         self.B = B
         self.observation_class = observation_class
         self.initial_state_flag = False
-
     def __str__(self):
         return "%s %s" % (self.id, self.observation_class)
-
     def as_array(self):
         return numpy.array([self.I, self.A, self.D1, self.D2, self.B])
-
     def get_id(self):
         return self.id
-
     def get_class(self):
         return self.observation_class
-
     def is_initial_state(self):
         return self.initial_state_flag
-
     def set_initial_state_flag(self):
         self.initial_state_flag = True
+    def as_dict(self):
+        return {'observation_class':self.get_class(),
+                'I':self.I, 'A':self.A, 'D1':self.D1, 'D2':self.D2, 'B':self.B}
 
-class StateIDCollection(object):
-    """docstring for StateIDCollection"""
-    def __init__(self):
-        super(StateIDCollection, self).__init__()
-        self.state_id_list = []
-    def __str__(self):
-        return str(self.state_id_list)
-    def __iter__(self):
-        for s in self.state_id_list:
-            yield s
-    def __contains__(self, state_id):
-        return (state_id in self.state_id_list)
-    def __len__(self):
-        return len(self.state_id_list)
-    def add_id(self, state_id):
-        self.state_id_list.append(state_id)
-    def as_list(self):
-        return self.state_id_list
 
 class BlinkModel(AggregatedKineticModel):
     '''
@@ -97,32 +74,10 @@ class BlinkModel(AggregatedKineticModel):
     def __init__(self, state_enumerator, route_mapper, parameter_set):
         super(BlinkModel, self).__init__(state_enumerator, route_mapper,
                                           parameter_set)
-        self.dark_state_id_collection = None
-        self.bright_state_id_collection = None
-        self._fill_state_id_collections()
-
-    def _fill_state_id_collections(self):
-        self.dark_state_id_collection = StateIDCollection()
-        for dark_state in self.iter_dark_states():
-            self.dark_state_id_collection.add_id( dark_state.get_id() )
-        self.bright_state_id_collection = StateIDCollection()
-        for bright_state in self.iter_bright_states():
-            self.bright_state_id_collection.add_id( bright_state.get_id() )
-
-    def iter_dark_states(self):
-        dark_inds = self.class_indices_dict['dark']
-        for ind in dark_inds:
-            s = self.states[ind]
-            yield s
-
-    def iter_bright_states(self):
-        bright_inds = self.class_indices_dict['bright']
-        for ind in bright_inds:
-            s = self.states[ind]
-            yield s
+        self.all_inactive_state_id = self.initial_state_id
 
     def get_initial_probability_vector(self):
-        initial_prob_vec = make_prob_vec_from_state_ids(
-                            self.dark_state_id_collection)
+        dark_state_id_collection = self.state_ids_by_class_dict['dark']
+        initial_prob_vec = make_prob_vec_from_state_ids(dark_state_id_collection)
         initial_prob_vec.set_state_probability(self.all_inactive_state_id, 1.0)
         return initial_prob_vec
