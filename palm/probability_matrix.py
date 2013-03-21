7332e8ec6c6b1f980544d638566cf048f3b085c9
Geoff Rollins <grollins@gmail.com>
2013-03-20 22:38:53 -0700

    Move new rate matrix class to rate matrix module.

--- palm/probability_matrix.py
+++ palm/probability_matrix.py
@@ -19,23 +19,6 @@ def make_prob_matrix_from_panda_data_frame(data_frame):
     pm.data_frame = data_frame
     return pm
 
-def make_rate_matrix_from_state_ids(index_id_collection,
-                                    column_id_collection=None):
-    pm = RateMatrix()
-    index_id_list = index_id_collection.as_list()
-    if column_id_collection:
-        column_id_list = column_id_collection.as_list()
-    else:
-        column_id_list = index_id_list
-    pm.data_frame = DataFrame(0.0, index=index_id_list,
-                              columns=column_id_list)
-    return pm
-
-def make_rate_matrix_from_panda_data_frame(data_frame):
-    pm = RateMatrix()
-    pm.data_frame = data_frame
-    return pm
-
 class ProbabilityMatrix(object):
     """docstring for ProbabilityMatrix"""
     def __init__(self):
@@ -55,22 +38,3 @@ class ProbabilityMatrix(object):
     def as_npy_array(self):
         return self.data_frame.values
 
-
-class RateMatrix(object):
-    """docstring for RateMatrix"""
-    def __init__(self):
-        super(RateMatrix, self).__init__()
-        self.date_frame = None
-    def __str__(self):
-        return str(self.data_frame)
-    def set_rate(self, state_id1, state_id2, rate):
-        self.data_frame.set_value(index=state_id1,col=state_id2, value=rate)
-    def get_rate(self, state_id1, state_id2):
-        return self.data_frame.get_value(index=state_id1, col=state_id2)
-    def balance_transition_rates(self):
-        # set diagonals to -sum of other entries in row
-        diagonal_inds = numpy.diag_indices_from(self.data_frame.values)
-        sum_along_row_series = self.data_frame.sum(1)
-        self.data_frame.values[diagonal_inds] = -sum_along_row_series
-    def as_npy_array(self):
-        return self.data_frame.values
--- palm/rate_matrix.py
+++ palm/rate_matrix.py
@@ -1,5 +1,51 @@
 import numpy
+from pandas import DataFrame
 from palm.util import DATA_TYPE
+
+def make_rate_matrix_from_state_ids(index_id_collection,
+                                    column_id_collection=None):
+    pm = RateMatrix()
+    index_id_list = index_id_collection.as_list()
+    if column_id_collection:
+        column_id_list = column_id_collection.as_list()
+    else:
+        column_id_list = index_id_list
+    pm.data_frame = DataFrame(0.0, index=index_id_list,
+                              columns=column_id_list)
+    return pm
+
+def make_rate_matrix_from_panda_data_frame(data_frame):
+    pm = RateMatrix()
+    pm.data_frame = data_frame
+    return pm
+
+
+class RateMatrix(object):
+    """docstring for RateMatrix"""
+    def __init__(self):
+        super(RateMatrix, self).__init__()
+        self.date_frame = None
+    def __len__(self):
+        return len(self.data_frame)
+    def __str__(self):
+        return str(self.data_frame)
+    def set_rate(self, state_id1, state_id2, rate):
+        self.data_frame.set_value(index=state_id1,col=state_id2, value=rate)
+    def get_rate(self, state_id1, state_id2):
+        return self.data_frame.get_value(index=state_id1, col=state_id2)
+    def balance_transition_rates(self):
+        # set diagonals to -sum of other entries in row
+        diagonal_inds = numpy.diag_indices_from(self.data_frame.values)
+        sum_along_row_series = self.data_frame.sum(1)
+        self.data_frame.values[diagonal_inds] = -sum_along_row_series
+    def as_npy_array(self):
+        return self.data_frame.values
+    def get_submatrix(self, index_id_collection, column_id_collection):
+        sub_df = self.data_frame.reindex(
+                    index=index_id_collection.as_list(),
+                    columns=column_id_collection.as_list())
+        return make_rate_matrix_from_panda_data_frame(sub_df)
+
 
 class RateMatrixFactory(object):
     """
@@ -66,8 +112,11 @@ class AggregatedRateMatrix(object):
                 self.rate_matrix[i,i] = -numpy.sum(self.rate_matrix[i,:])
             self.is_finalized = True
 
-    def get_size(self):
-        return self.rate_matrix.shape[0]
+    def get_size(self, axis=0):
+        return self.rate_matrix.shape[axis]
 
     def as_numpy_array(self):
         return self.rate_matrix
+
+    def get_diagonal_vector(self):
+        return self.rate_matrix.diagonal()
