77d909da42aa853515f88518ab1b1a98978b12a5
Geoff Rollins <grollins@gmail.com>
2013-03-26 09:27:32 -0700

    Implement vector and matrix trajectories for saving intermediate results during likelihood calculation for easier debugging.

--- palm/probability_vector.py
+++ palm/probability_vector.py
@@ -30,6 +30,33 @@ class ProbabilityVector(object):
         return self.series.sum()
     def scale_vector(self, scale_factor):
         self.series *= scale_factor
-    def get_ml_state(self):
-        # print self.series
-        return self.series.idxmax()
+    def get_ml_state_series(self, num_states):
+        ordered_series = self.series.order(ascending=False)
+        upper_limit = min(num_states, len(ordered_series))
+        return ordered_series[:upper_limit]
+    def combine_first(self, vec):
+        # self clobbers vec
+        return self.series.combine_first(vec.series)
+
+
+class VectorTrajectory(object):
+    """docstring for VectorTrajectory"""
+    def __init__(self, state_id_collection):
+        super(VectorTrajectory, self).__init__()
+        self.state_id_collection = state_id_collection
+        self.vec_list = []
+    def __len__(self):
+        return len(self.vec_list)
+    def __str__(self):
+        full_str = ""
+        for v in iter(self):
+            full_str += str(v)
+            full_str += "\n"
+        return full_str
+    def __iter__(self):
+        for v in iter(self.vec_list):
+            yield v
+    def add_vector(self, vec):
+        vec_template = make_prob_vec_from_state_ids(self.state_id_collection)
+        combined_vec = vec.combine_first(vec_template)
+        self.vec_list.append(combined_vec)
--- palm/rate_matrix.py
+++ palm/rate_matrix.py
@@ -29,8 +29,15 @@ class RateMatrix(object):
         return len(self.data_frame)
     def __str__(self):
         return str(self.data_frame)
+    def __iter__(self):
+        for column_id, row_series in self.data_frame.iteritems():
+            yield column_id, row_series
+    def get_shape(self):
+        return self.data_frame.shape
+    def get_value(self, index, column):
+        return self.data_frame.get_value(index, column)
     def set_rate(self, state_id1, state_id2, rate):
-        self.data_frame.set_value(index=state_id1,col=state_id2, value=rate)
+        self.data_frame.set_value(index=state_id1, col=state_id2, value=rate)
     def get_rate(self, state_id1, state_id2):
         return self.data_frame.get_value(index=state_id1, col=state_id2)
     def balance_transition_rates(self):
@@ -45,78 +52,19 @@ class RateMatrix(object):
                     index=index_id_collection.as_list(),
                     columns=column_id_collection.as_list())
         return make_rate_matrix_from_panda_data_frame(sub_df)
+    def get_index_id_list(self):
+        return self.data_frame.index.tolist()
 
 
-class RateMatrixFactory(object):
-    """
-    This factory class creates a rate matrix for an
-    aggregated kinetic model. States and routes must
-    be defined in order to create the matrix.
-    """
-    def __init__(self, rate_matrix_class):
-        super(RateMatrixFactory, self).__init__()
-        self.rate_matrix_class = rate_matrix_class
-
-    def create_rate_matrix(self, model_size, model_routes, state_index_dict,
-                            class_indices_dict, time):
-        rate_matrix = self.rate_matrix_class( model_size, class_indices_dict )
-        for r in model_routes:
-            start_index = state_index_dict[r.start_state]
-            end_index = state_index_dict[r.end_state]
-            this_log_rate = r.compute_log_rate(t=time)
-            this_rate = 10**(this_log_rate)
-            assert this_rate >= 0.0, "%d %d %s" % (start_index, end_index,
-                                                   str(this_rate))
-            rate_matrix.set_rate(start_index, end_index, this_rate)
-        rate_matrix.finalize_matrix()
-        return rate_matrix
-
-    def create_submatrix(self, rate_matrix, start_class, end_class):
-        row_inds = rate_matrix.class_indices_dict[start_class]
-        col_inds = rate_matrix.class_indices_dict[end_class]
-        numpy_submatrix = rate_matrix.rate_matrix[row_inds[0]:row_inds[-1]+1,
-                                                  col_inds[0]:col_inds[-1]+1]
-        numpy_submatrix = numpy.atleast_2d(numpy_submatrix)
-        submatrix = self.rate_matrix_class(
-                        model_size=1, class_indices_dict=None )
-        submatrix.rate_matrix = numpy_submatrix
-        return submatrix
-
-
-class AggregatedRateMatrix(object):
-    """
-    A rate matrix for a model with discrete observation classes.
-    The states are grouped by observation class to give the matrix
-    the following sub-matrix structure:
-           Q_dd | Q_db
-     Q =   -----------
-           Q_bd | Q_bb
-    """
-    def __init__(self, model_size, class_indices_dict):
-        super(AggregatedRateMatrix, self).__init__()
-        self.rate_matrix = numpy.zeros((model_size,model_size), dtype=DATA_TYPE)
-        self.class_indices_dict = class_indices_dict
-        self.is_finalized = False
-
-    def set_rate(self, start, end, rate):
-        if self.is_finalized:
-            assert False, "Rate matrix is already finalized."
-        else:
-            self.rate_matrix[start, end] = rate
-
-    def finalize_matrix(self):
-        if self.is_finalized:
-            assert False, "Rate matrix is already finalized."
-        else:
-            for i in xrange(self.rate_matrix.shape[0]):
-                self.rate_matrix[i,i] = -numpy.sum(self.rate_matrix[i,:])
-            self.is_finalized = True
-
-    def get_size(self, axis=0):
-        return self.rate_matrix.shape[axis]
-
-    def as_numpy_array(self):
-        return self.rate_matrix
-
-    def get_diagonal_vector(self):
-        return self.rate_matrix.diagonal()
+class RateMatrixTrajectory(object):
+    """docstring for RateMatrixTrajectory"""
+    def __init__(self):
+        super(RateMatrixTrajectory, self).__init__()
+        self.matrix_list = []
+    def __iter__(self):
+        for m in iter(self.matrix_list):
+            yield m
+    def __len__(self):
+        return len(self.matrix_list)
+    def add_matrix(self, rate_matrix):
+        self.matrix_list.append(rate_matrix)
