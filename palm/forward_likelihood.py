ee55cca4d0173afbf4097d4cbc5afb1ebb6f8638
Geoff Rollins <grollins@gmail.com>
2013-03-26 09:29:54 -0700

    Implement local likelihood method, which only tracks a subset of states at each step in the calculation. The goal is to shrink the size of the matrices to speed up the matrix exponential.

--- palm/forward_likelihood.py
+++ palm/forward_likelihood.py
@@ -3,34 +3,130 @@ import cPickle
 from palm.base.data_predictor import DataPredictor
 from palm.likelihood_prediction import LikelihoodPrediction
 from palm.forward_calculator import ForwardCalculator
-from palm.expm import ScipyMatrixExponential
+from palm.linalg import ScipyMatrixExponential
+from palm.probability_vector import VectorTrajectory
+from palm.rate_matrix import RateMatrixTrajectory
 from palm.util import ALMOST_ZERO, DATA_TYPE
 
-class ForwardPredictor(DataPredictor):
-    """docstring for ForwardPredictor"""
-    def __init__(self, always_rebuild_rate_matrix):
-        super(ForwardPredictor, self).__init__()
-        self.always_rebuild_rate_matrix = always_rebuild_rate_matrix
+class LocalPredictor(DataPredictor):
+    """docstring for LocalPredictor"""
+    def __init__(self, depth, num_tracked_states, archive_matrices=False):
+        super(LocalPredictor, self).__init__()
+        self.depth = depth
+        self.num_tracked_states = num_tracked_states
+        self.archive_matrices = archive_matrices
         expm_calculator = ScipyMatrixExponential()
         self.forward_calculator = ForwardCalculator(expm_calculator)
         self.prediction_factory = LikelihoodPrediction
-
+        self.vector_trajectory = None
+        self.rate_matrix_trajectory = None
+        self.scaling_factor_set = None
     def predict_data(self, model, trajectory):
-        scaling_factor_set = self.compute_forward_vectors(model, trajectory)
-        likelihood = 1./(scaling_factor_set.compute_product())
+        self.scaling_factor_set = self.compute_likelihood(model, trajectory)
+        likelihood = 1./(self.scaling_factor_set.compute_product())
         if likelihood < ALMOST_ZERO:
             likelihood = ALMOST_ZERO
         log_likelihood = numpy.log10(likelihood)
         return self.prediction_factory(log_likelihood)
+    def compute_likelihood(self, model, trajectory):
+        self.vector_trajectory = VectorTrajectory(model.state_id_collection)
+        if self.archive_matrices:
+            self.rate_matrix_trajectory = RateMatrixTrajectory()
+        else:
+            pass
+        # initialize probability vector
+        scaling_factor_set = ScalingFactorSet()
+        init_prob_vec = model.get_initial_probability_vector()
+        scaling_factor_set.scale_vector(init_prob_vec)
+        prev_alpha = init_prob_vec
+        self.vector_trajectory.add_vector(init_prob_vec)
+        rate_matrix_organizer = RateMatrixOrganizer(model)
 
+        # loop through trajectory segments, compute likelihood for each segment
+        for segment_number, segment in enumerate(trajectory):
+            # get current segment class and duration
+            cumulative_time = trajectory.get_cumulative_time(segment_number)
+            segment_duration = segment.get_duration()
+            start_class = segment.get_class()
+            # get next segment class (if there is a next one)
+            next_segment = trajectory.get_segment(segment_number + 1)
+            if next_segment:
+                end_class = next_segment.get_class()
+            else:
+                end_class = None
+            ml_state_series = prev_alpha.get_ml_state_series(
+                                self.num_tracked_states)
+            rate_matrix_organizer.build_local_rate_matrix(
+                                    cumulative_time, ml_state_series,
+                                    depth=self.depth)
+            if self.archive_matrices:
+                self.rate_matrix_trajectory.add_matrix(
+                        rate_matrix_organizer.rate_matrix)
+            else:
+                pass
+            rate_matrix_aa = rate_matrix_organizer.get_local_submatrix(
+                                    start_class, start_class)
+            rate_matrix_ab = rate_matrix_organizer.get_local_submatrix(
+                                    start_class, end_class)
+            localized_prev_alpha = rate_matrix_organizer.get_local_vec(
+                                    rate_matrix_aa, ml_state_series)
+            alpha = self._compute_alpha( rate_matrix_aa, rate_matrix_ab,
+                                         segment_number, segment_duration,
+                                         start_class, end_class,
+                                         localized_prev_alpha)
+            # scale probability vector to avoid numerical underflow
+            scaled_alpha = scaling_factor_set.scale_vector(alpha)
+            # store handle to current alpha vector for next iteration
+            prev_alpha = scaled_alpha
+            # archive the current alpha vector for later inspection
+            self.vector_trajectory.add_vector(scaled_alpha)
+        # end for loop
+        return scaling_factor_set
+    def _compute_alpha(self, rate_matrix_aa, rate_matrix_ab, segment_number,
+                       segment_duration, start_class, end_class, prev_alpha):
+        '''
+        prev alpha (1, N) 2-d array
+        '''
+        alpha = self.forward_calculator.compute_forward_vector(
+                    prev_alpha, rate_matrix_aa, rate_matrix_ab,
+                    segment_duration)
+        return alpha
+
+
+class ForwardPredictor(DataPredictor):
+    """docstring for ForwardPredictor"""
+    def __init__(self, always_rebuild_rate_matrix, archive_matrices=False):
+        super(ForwardPredictor, self).__init__()
+        self.always_rebuild_rate_matrix = always_rebuild_rate_matrix
+        self.archive_matrices = archive_matrices
+        expm_calculator = ScipyMatrixExponential()
+        self.forward_calculator = ForwardCalculator(expm_calculator)
+        self.prediction_factory = LikelihoodPrediction
+        self.vector_trajectory = None
+        self.rate_matrix_trajectory = None
+        self.scaling_factor_set = None
+    def predict_data(self, model, trajectory):
+        self.scaling_factor_set = self.compute_forward_vectors(
+                                    model, trajectory)
+        likelihood = 1./(self.scaling_factor_set.compute_product())
+        if likelihood < ALMOST_ZERO:
+            likelihood = ALMOST_ZERO
+        log_likelihood = numpy.log10(likelihood)
+        return self.prediction_factory(log_likelihood)
     def compute_forward_vectors(self, model, trajectory):
+        self.vector_trajectory = VectorTrajectory(model.state_id_collection)
+        if self.archive_matrices:
+            self.rate_matrix_trajectory = RateMatrixTrajectory()
+        else:
+            pass
         # initialize probability vector
         scaling_factor_set = ScalingFactorSet()
         rate_matrix_organizer = RateMatrixOrganizer(model)
         rate_matrix_organizer.build_rate_matrix(time=0.0)
         init_prob = model.get_initial_probability_vector()
-        scaled_init_prob = scaling_factor_set.scale_vector(init_prob)
-        prev_alpha = scaled_init_prob
+        scaling_factor_set.scale_vector(init_prob)
+        self.vector_trajectory.add_vector(init_prob)
+        prev_alpha = init_prob
 
         # loop through trajectory segments, compute likelihood for each segment
         for segment_number, segment in enumerate(trajectory):
@@ -56,7 +152,11 @@ class ForwardPredictor(DataPredictor):
                                     start_class, start_class)
                 rate_matrix_ab = rate_matrix_organizer.get_submatrix(
                                     start_class, end_class)
-
+            if self.archive_matrices:
+                self.rate_matrix_trajectory.add_matrix(
+                        rate_matrix_organizer.rate_matrix)
+            else:
+                pass
             alpha = self._compute_alpha( rate_matrix_aa, rate_matrix_ab,
                                          segment_number, segment_duration,
                                          start_class, end_class,
@@ -66,6 +166,7 @@ class ForwardPredictor(DataPredictor):
             scaled_alpha = scaling_factor_set.scale_vector(alpha)
             # store handle to current alpha vector for next iteration
             prev_alpha = scaled_alpha
+            self.vector_trajectory.add_vector(scaled_alpha)
         # end for loop
         return scaling_factor_set
 
@@ -77,8 +178,6 @@ class ForwardPredictor(DataPredictor):
         alpha = self.forward_calculator.compute_forward_vector(
                     prev_alpha, rate_matrix_aa, rate_matrix_ab,
                     segment_duration)
-        assert alpha.shape[0] == 1, str(alpha.shape)
-        assert alpha.shape[1] >= 1, str(alpha.shape)
         return alpha
 
 
@@ -89,6 +188,8 @@ class ScalingFactorSet(object):
         return len(self.factor_list)
     def __str__(self):
         return str(self.factor_list)
+    def get_factor_set(self):
+        return self.factor_list
     def append(self, factor):
         self.factor_list.append(factor)
     def compute_product(self):
@@ -98,12 +199,11 @@ class ScalingFactorSet(object):
         vector_sum = vector.sum_vector()
         if vector_sum < ALMOST_ZERO:
             this_scaling_factor = 1./ALMOST_ZERO
-            scaled_vector = numpy.ones_like(vector)
         else:
             this_scaling_factor = 1./vector_sum
-            scaled_vector = vector.scale_vector(this_scaling_factor)
+        vector.scale_vector(this_scaling_factor)
         self.append(this_scaling_factor)
-        return scaled_vector
+        return vector
 
 
 class RateMatrixOrganizer(object):
@@ -111,14 +211,37 @@ class RateMatrixOrganizer(object):
     def __init__(self, model):
         super(RateMatrixOrganizer, self).__init__()
         self.model = model
-
+        self.rate_matrix = None
     def build_rate_matrix(self, time):
-        self.model.build_rate_matrix(time=time)
+        self.rate_matrix = self.model.build_rate_matrix(time=time)
         return
-
     def get_submatrix(self, start_class, end_class):
         if start_class and end_class:
-            submatrix = self.model.get_submatrix(start_class, end_class)
+            submatrix = self.model.get_submatrix(
+                            self.rate_matrix, start_class, end_class)
         else:
             submatrix = None
         return submatrix
+    def build_local_rate_matrix(self, time, start_state_series, depth):
+        self.rate_matrix = self.model.get_local_matrix(
+                            time, start_state_series, depth)
+    def get_local_submatrix(self, start_class, end_class):
+        if start_class and end_class:
+            submatrix = self.model.get_local_submatrix(
+                            self.rate_matrix, start_class, end_class)
+        else:
+            submatrix = None
+        return submatrix
+    def get_local_vec(self, local_rate_matrix, start_state_series):
+        try:
+            local_vec = self.model.get_local_vec(
+                            local_rate_matrix, start_state_series)
+        except KeyError:
+            print "Q"
+            print self.rate_matrix
+            print "Q_local"
+            print local_rate_matrix
+            print "series"
+            print start_state_series
+            raise
+        return local_vec
--- palm/test/test_forward_likelihood.py
+++ palm/test/test_forward_likelihood.py
@@ -1,18 +1,17 @@
 import os.path
 import nose.tools
 import numpy
-from palm.forward_likelihood import ForwardPredictor
-from palm.backward_likelihood import BackwardPredictor
+from palm.forward_likelihood import ForwardPredictor, LocalPredictor
 from palm.blink_factory import SingleDarkBlinkFactory
 from palm.blink_parameter_set import SingleDarkParameterSet
 from palm.likelihood_judge import LikelihoodJudge
 from palm.blink_target_data import BlinkTargetData
 
 @nose.tools.istest
-def computes_same_likelihood_as_backward_predictor():
+def local_computes_same_likelihood_as_full_predictor():
     model_factory = SingleDarkBlinkFactory(MAX_A=5)
     model_parameters = SingleDarkParameterSet()
-    model_parameters.set_parameter('N', 5)
+    model_parameters.set_parameter('N', 3)
     model_parameters.set_parameter('log_ka', -0.5)
     model_parameters.set_parameter('log_kd', -0.5)
     model_parameters.set_parameter('log_kr', -0.5)
@@ -27,8 +26,7 @@ def computes_same_likelihood_as_backward_predictor():
     model = model_factory.create_model(model_parameters)
     trajectory = target_data.get_feature()
     forward_prediction = forward_predictor.predict_data(model, trajectory)
-    print forward_prediction
-    backward_predictor = BackwardPredictor(always_rebuild_rate_matrix=False)
-    backward_prediction = backward_predictor.predict_data(model, trajectory)
-    print backward_prediction
-    nose.tools.eq_(forward_prediction, backward_prediction)
+    local_predictor = LocalPredictor(depth=3, num_tracked_states=5)
+    local_prediction = local_predictor.predict_data(model, trajectory)
+    print forward_prediction, local_prediction
+    nose.tools.ok_(abs(forward_prediction.compute_difference(local_prediction)) < 1.0)
